22757447fd45c1e9dff8b6bf8e0efb00c439097e
urbanit <sroussos@urbanit.gr>
2015-11-03 13:10:51 +0200

    Init Commit

---
+++ Urbanit.CultureLayerRule/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Urbanit.CultureLayerRule")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Urbanit")]
+[assembly: AssemblyProduct("Orchard")]
+[assembly: AssemblyCopyright("Copyright © Urbanit 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4cdccda9-25ff-4235-953f-69b5e34c488c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.9.0.0")]
+[assembly: AssemblyFileVersion("1.9.0.0")]
+
---
+++ Urbanit.CultureLayerRule/RuleEngine/CultureRuleProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Orchard;
+using Orchard.Widgets.Services;
+
+namespace Urbanit.CultureLayerRule.RuleEngine
+{
+    public class CulturedRuleProvider : IRuleProvider {
+
+        private readonly IOrchardServices _services;
+        public CulturedRuleProvider(IOrchardServices services)
+        {
+            _services = services;
+        }
+
+        public void Process(RuleContext ruleContext) {
+            if (!String.Equals(ruleContext.FunctionName, "culture", StringComparison.OrdinalIgnoreCase)) {
+                return;
+            }
+
+            var ruleCulture = Convert.ToString(ruleContext.Arguments[0]);
+
+            IWorkContextAccessor ContextManager = _services.WorkContext.Resolve<IWorkContextAccessor>(); 
+            var siteCulture = ContextManager.GetContext().CurrentCulture;
+
+
+            ruleContext.Result = String.Equals(ruleCulture, siteCulture, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
